3de5599d305531804c5cd22405f176ccc75d7421
youxiaoxian <1092939085@qq.com>
2021-04-12 17:28:14 +0800

    fixture

--- test2/conftest.py
+++ test2/conftest.py
@@ -3,15 +3,15 @@ import pytest
 from test2.Calculator import Calculator
 from test2.test_calc import get_param
 
-
+#fixture实现set up和tear down
 @pytest.fixture(scope='class')
 def inicalc_class():
-    print("set up")
+    print("计算开始")
     calc = Calculator()
     yield calc
-    print("tear down")
+    print("计算结束")
 
-
+#fixture实现参数化
 @pytest.fixture(params=get_param()['add_int'], ids=get_param()['ids'])
 def get_add_int_param_calc(request):
     return request.param
