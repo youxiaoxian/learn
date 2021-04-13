a97d51080ff4365fda7d20bcf0a3b8cb0f6c789f
youxiaoxian <1092939085@qq.com>
2021-04-13 11:39:45 +0800

    第二次作业

--- test2/conftest.py
+++ test2/conftest.py
@@ -30,3 +30,14 @@ def get_div_int_param_calc(request):
 @pytest.fixture(params=get_param()['div_float'], ids=get_param()['ids'])
 def get_div_float_param_calc(request):
     return request.param
+
+#hook函数
+def pytest_collection_modifyitems(session, config, items: list):
+    print("这是收集所有测试用例的方法")
+    print(items)
+    items.reverse()
+    for item in items:
+        item.name = item.name.encode('utf-8').decode('unicode-escape')
+        print(item.name)
+        item._nodeid = item._nodeid.encode('utf-8').decode('unicode-escape')
+        print(item._nodeid)
--- test2/test_calc.py
+++ test2/test_calc.py
@@ -1,30 +1,46 @@
 from decimal import Decimal
+
+import allure
+import pytest
 import yaml
 
 def get_param():
-    with open("conf/param.yml") as f:
-        param = yaml.safe_load(f)
+    with open("conf/param.yml",encoding='utf-8') as f: #mac默认是utf-8，而win默认是gbk，所有win执行需加encoding='utf-8'
+        param = yaml.safe_load(f) #safe_load 将utf-8转换为unicode，用hook函数把unicode编码成utf8然后再解码成中文编码
     return param
 
+
+@allure.feature("计算器")
 class TestCal:
 
-    def test_add_int(self,get_add_int_param_calc,inicalc_class):
+    # @pytest.mark.second
+    @pytest.mark.run(order=2)
+    @allure.story("整数相加")
+    def test_add_int(self, get_add_int_param_calc, inicalc_class):
         assert get_add_int_param_calc[2] == inicalc_class.add(get_add_int_param_calc[0], get_add_int_param_calc[1])
 
+    @pytest.mark.run(order=1)
+    # @pytest.mark.first
+    @allure.story("小数相加")
+    def test_add_float(self, get_add_float_param_calc, inicalc_class):
+        assert get_add_float_param_calc[2] == round(
+            inicalc_class.add(get_add_float_param_calc[0], get_add_float_param_calc[1]), 2)
 
-    def test_add_float(self, get_add_float_param_calc,inicalc_class):
-        assert get_add_float_param_calc[2] == round(inicalc_class.add(get_add_float_param_calc[0], get_add_float_param_calc[1]),2)
-
-
-    def test_div_int(self, get_div_int_param_calc,inicalc_class):
+    @allure.story("整数相除")
+    @pytest.mark.run(order=3)
+    def test_div_int(self, get_div_int_param_calc, inicalc_class):
         try:
-            assert Decimal(get_div_int_param_calc[2]).quantize(Decimal("0.00")) == Decimal(inicalc_class.div(get_div_int_param_calc[0], get_div_int_param_calc[1])).quantize(Decimal("0.00"))
+            assert Decimal(get_div_int_param_calc[2]).quantize(Decimal("0.00")) == Decimal(
+                inicalc_class.div(get_div_int_param_calc[0], get_div_int_param_calc[1])).quantize(Decimal("0.00"))
         except ZeroDivisionError:
             print('ZeroDivisionError')
 
-
-    def test_div_float(self,get_div_float_param_calc,inicalc_class):
+    # @pytest.mark.last
+    @pytest.mark.run(order=-1)
+    @allure.story("小数相除")
+    def test_div_float(self, get_div_float_param_calc, inicalc_class):
         try:
-            assert Decimal(get_div_float_param_calc[2]).quantize(Decimal("0.00")) == Decimal(inicalc_class.div(get_div_float_param_calc[0], get_div_float_param_calc[1])).quantize(Decimal("0.00"))
+            assert Decimal(get_div_float_param_calc[2]).quantize(Decimal("0.00")) == Decimal(
+                inicalc_class.div(get_div_float_param_calc[0], get_div_float_param_calc[1])).quantize(Decimal("0.00"))
         except ZeroDivisionError:
             print('ZeroDivisionError')
