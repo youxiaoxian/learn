399705df48dcdbb6a371472d2c9cd17721820fd3
youxiaoxian <1092939085@qq.com>
2021-04-16 17:25:38 +0800

    学习获取cookie，cookie登录

--- test3/test_wework.py
+++ test3/test_wework.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 
 class TestWework:
     def test_wework(self):
+        # 复用只支持chrome浏览器
         options=webdriver.ChromeOptions()
+        # 设置debug地址
         options.debugger_address='127.0.0.1:9222'
         driver=webdriver.Chrome(options=options)
         # driver.get('https://work.weixin.qq.com/wework_admin/frame#index')
@@ -12,8 +14,14 @@ class TestWework:
         # time.sleep(5)
         # print(driver.page_source)
         # driver.find_element_by_id("menu_contacts").click()
+        # 在当前页面点击通讯录
         driver.find_element_by_xpath('//*[@id="menu_contacts"]/span').click()
+        time.sleep(5)
+        #在通讯录页面点击添加成员
+        driver.find_element_by_xpath('//*[@id="js_contacts41"]/div/div[2]/div/div[2]/div[3]/div[1]/a[1]').click()
+        # 获取cookie信息
         cookie = driver.get_cookies()
+        # 把cookie存如yaml文件内
         with open("data.yaml","w",encoding="UTF-8") as f:
              yaml.dump(cookie,f)
 
