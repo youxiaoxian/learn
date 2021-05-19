29041872ff367fbfb99460bf2ab8ef07043fb0b9
youxiaoxian <1092939085@qq.com>
2021-05-19 13:14:48 +0800

    requests

---
+++ api/test_api.py
@@ -0,0 +1,22 @@
+import json
+
+import requests
+class TestAPI:
+    def test_api(self):
+        # r = requests.get('https://httpbin.testing-studio.com/get')
+        data = {
+            "appid": "test002",
+            "startTime": "2021-05-05 00:00:00",
+            "endTime": "2021-05-12 14:59:59",
+            "curpage": "1",
+            "pagesize": "50"
+        }
+        # headers = {
+        #     'Content-Type': 'application/json'
+        # }
+        r = requests.post('http://192.168.7.147:22010/dataanalysis/gwsummaryanalysis',json=data)
+        print(r.status_code)
+        print(json.dumps(r.json(),indent=2,ensure_ascii=False))
+        print(type(r.json()))
+        print(r.text)
+        print(type(r.text))
